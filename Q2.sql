58acb6bcfde4e2d5c8f34e0d979c98e7bfceb591
AIsacson <annaisacson@Annas-MacBook-Pro.local>
2017-05-10 14:33:39 +0200

    Final update

--- Q2.sql
+++ Q2.sql
@@ -1,7 +1,9 @@
-SELECT XMLELEMENT(NAME "Resultat", 
-		XMLAGG(XMLELEMENT(NAME "Förlag",XMLATTRIBUTES(name AS "Namn", country AS "Land"),
-			(SELECT XMLAGG(XMLELEMENT(NAME "Bok", XMLATTRIBUTES(title AS "Titel", genre AS "Genre")))
-		 	 FROM book, edition, XMLTABLE('distinct-values($TRANSLATIONS//Translation/@Publisher)' COLUMNS "publisher" VARCHAR(25) PATH 'string()') AS pub
-		  	 WHERE book.id = edition.book
-		 	 AND publisher.name = pub."publisher"))))
-FROM publisher;
+SELECT XMLELEMENT(NAME "Resultat", XMLAGG(xt))
+FROM (SELECT XMLELEMENT(NAME "Förlag",XMLATTRIBUTES(name AS "Namn", country AS "Land"),
+			 XMLAGG(XMLELEMENT(NAME "Bok", XMLATTRIBUTES(title AS "Titel", genre AS "Genre")))) AS xt
+FROM publisher, book
+WHERE publisher.name IN (SELECT pub."publisher"
+					   FROM edition, XMLTABLE('distinct-values($TRANSLATIONS//Translation/@Publisher)' 
+					   				 COLUMNS "publisher" VARCHAR(25) PATH 'string()') AS pub
+					   WHERE book.id = edition.book)
+GROUP BY name, country);
