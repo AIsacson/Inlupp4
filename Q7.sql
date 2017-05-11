80ee8688000b2e05608fbc17103a06ed9327ac63
AIsacson <aisacson@kth.se>
2017-05-11 17:25:08 +0200

    Update Q7.sql

--- Q7.sql
+++ Q7.sql
@@ -1,7 +1,10 @@
-SELECT XMLELEMENT(NAME "Förlag", XMLATTRIBUTES(name AS "namn", country AS "land"),
-	XMLAGG(XMLELEMENT(NAME "Språk", "sprak")))
-FROM publisher, edition, XMLTABLE('$TRANSLATIONS//Translation[not(@Language = .)]' 
-						 COLUMNS "sprak" VARCHAR(25) PATH '@Language',
-								 "forlag" VARCHAR(25) PATH '@Publisher') AS s
-WHERE "forlag" = publisher.name
-GROUP BY name, country;
+CREATE VIEW f AS
+SELECT XMLELEMENT(NAME "Alla", XMLAGG(XMLELEMENT(NAME "Förlag", XMLATTRIBUTES(name AS "Namn", country AS "Land")))) AS "Publishers"
+FROM publisher
+
+SELECT XMLELEMENT(NAME "Resultat",(XMLQUERY('for $forlag in $f//Förlag
+					     let $sprak := for $s in distinct-values($t//Translation[@Publisher = $forlag/@Namn]/@Language)
+							   return element Språk {$s}
+					     return element Förlag{$forlag/@Namn, $forlag/@Land, $sprak}'
+				   	     PASSING XMLAGG("Publishers") AS "f", XMLAGG(Translations) AS "t")))AS "Förlag"
+FROM edition, forlag
