d7ad110956994a2ec9c98eda418c4e8f847d25e6
AIsacson <aisacson@kth.se>
2017-05-11 17:34:03 +0200

    Final update

--- Q8.sql
+++ Q8.sql
@@ -1,8 +1,9 @@
 UPDATE edition
-SET translations = XMLQUERY('transform copy $res := $TRANSLATIONS 
-								modify do replace $res/Translation[@Language = "French"]/@Publisher with attribute Publisher {"ABC International"}
-								return $res')
+SET translations = XMLQUERY('transform
+			     copy $res := $TRANSLATIONS 
+			     modify do replace value of $res//Translation[@Language = "French"]/@Publisher with "ABC International"
+			     return $res')
 WHERE year = 1999
 AND book IN (SELECT id
-			 FROM book
-			 WHERE title = 'Archeology in Egypt')
+	     FROM book
+ 	     WHERE title = 'Archeology in Egypt')
