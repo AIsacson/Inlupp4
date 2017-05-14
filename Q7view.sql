f40c6c31104439c354eef43c2d28752bc0bf14e3
AIsacson <aisacson@kth.se>
2017-05-14 15:37:39 +0200

    Update Q7view.sql

--- Q7view.sql
+++ Q7view.sql
@@ -1,3 +1,3 @@
-CREATE VIEW f AS
+CREATE VIEW forlag AS
 SELECT XMLELEMENT(NAME "Alla", XMLAGG(XMLELEMENT(NAME "Förlag", XMLATTRIBUTES(name AS "Namn", country AS "Land")))) AS "Publishers"
 FROM publisher
